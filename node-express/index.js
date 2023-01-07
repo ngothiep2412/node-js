e9d11d1f264b174d1c24c662de0ca501ab46e849
NgoThiep <ngothiep2412@gmail.com>
2023-01-07 14:07:38 +0700

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const http = require("http");
-
 const hostname = "localhost";
 const port = 5000;
 
 const app = express();
+app.use(morgan("dev"));
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
